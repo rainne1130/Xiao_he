ebf39308c55463166fe8bf00e440a0ab1d372492
rainne1130 <pain88_go@yahoo.com.tw>
2026-05-20 02:21:33 +0800

    Update nainaiwork.js

--- nainaiwork.js
+++ nainaiwork.js
@@ -594,6 +594,12 @@ client.on("messageCreate", async (message) => {
   if (content.includes("小賀")) {
  return message.reply("???怎麼了???要送我罐罐了嗎??");
   }
+  if (content.includes("汪")) {
+ return message.reply("你在狗叫什麼?");
+  }
+  if (content.includes("法克")) {
+	  return message.reply("真的是法克...");
+  }
 
 
 });
